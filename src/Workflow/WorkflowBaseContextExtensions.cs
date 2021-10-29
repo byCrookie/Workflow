dd0414c9692c3a72430cc2fb8c91c106b60260df
byCrookie <maeni.h01@hotmail.com>
2021-10-29 19:43:16 +0200

    Workflow Extensions

---
+++ src/Workflow/WorkflowBaseContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Workflow
+{
+    public static class WorkflowBaseContextExtensions
+    {
+        public static void MapTo(this WorkflowBaseContext origin, WorkflowBaseContext target)
+        {
+            target.Exception = origin.Exception;
+            target.IsStop = origin.IsStop;
+        }
+        
+        public static void SetException(this WorkflowBaseContext context, Exception exception)
+        {
+            context.Exception = exception;
+        }
+        
+        public static bool HasException(this WorkflowBaseContext context)
+        {
+            return context.Exception != null;
+        }
+        
+        public static bool IsStopped(this WorkflowBaseContext context)
+        {
+            return context.IsStop;
+        }
+    }
+}
